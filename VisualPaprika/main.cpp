303bd4871a66410436848fec6298637daf67a9a8
Stefan Dimitrijević <stefankl96@gmail.com>
2022-08-13 19:01:18 +0200

    Update main.cpp

--- VisualPaprika/main.cpp
+++ VisualPaprika/main.cpp
@@ -3,9 +3,44 @@
 #include <iostream>
 using namespace std;
 
+namespace visual_paprika
+{ 
+    namespace physical_environment
+    {
+        int width_mm, height_mm;
+    }
+
+    namespace glfw
+    {
+        void monitor_callback(GLFWmonitor* monitor, int event)
+        {
+            if (event == GLFW_CONNECTED)
+            {
+                // The monitor was connected
+            }
+            else if (event == GLFW_DISCONNECTED)
+            {
+                // The monitor was disconnected
+            }
+        }
+        
+        void PrintConsoleMonitorData()
+        {
+            GLFWmonitor* primary = glfwGetPrimaryMonitor();
+            glfwGetMonitorPhysicalSize(primary, &physical_environment::width_mm, &physical_environment::height_mm);
+            cout << physical_environment::width_mm << " " << physical_environment::height_mm << endl;
+        }
+    }
+    
+    
+}
+
+
+
+
 // main() is where program execution begins.
 int main() {
-   cout << "Hello World"; // prints Hello World
+   cout << "Hello World~"; // prints Hello World
    GLFWwindow * window;
    if( !glfwInit() )
    {
@@ -19,6 +54,10 @@ int main() {
       glfwTerminate();
       exit( EXIT_FAILURE );
    }
+
+
+   glfwSetMonitorCallback(visual_paprika::glfw::monitor_callback);
+   visual_paprika::glfw::PrintConsoleMonitorData();
    // Main loop
    while( !glfwWindowShouldClose(window) )
    {
