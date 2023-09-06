df0f55d36d9f9ae2c69baac6fa661dce4691191d
Stefan Dimitrijević <stefankl96@gmail.com>
2023-09-06 09:17:09 +0200

    vulkan windows

--- VisualPaprika/main.cpp
+++ VisualPaprika/main.cpp
@@ -1,4 +1,5 @@
 #include<GLFW/glfw3.h>
+#include<vulkan/vulkan.h>
 
 #include <iostream>
 using namespace std;
@@ -36,10 +37,45 @@ namespace visual_paprika
 }
 
 
+//Test vulkan build
+VkInstance instance;
+void createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        createInfo.enabledLayerCount = 0;
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
+        else
+        {
+            cout<<"vulkan works"<<endl;
+        }
+    }
 
 
 // main() is where program execution begins.
 int main() {
+
+    createInstance();
+
    cout << "Hello World~"; // prints Hello World
    GLFWwindow * window;
    if( !glfwInit() )
